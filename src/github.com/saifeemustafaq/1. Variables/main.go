50145b51b04cd0af64d248348b478355b4ea4bd1
Mustafa Saifee <saifeemustafaq@gmail.com>
2020-05-30 10:17:30 +0530

    learned variables in go

---
+++ src/github.com/saifeemustafaq/1. Variables/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var n string = "mustafa7@7" // globally declared variable
+
+var I int = 12 // this will not raise an error
+
+func main() {
+	var i int //initialized variable
+	i = 1     // declared variable
+	fmt.Println(i)
+
+	var j int = 12 //intialized and declared var
+	fmt.Println(j)
+
+	k := 123 // auto-detect variable type
+	fmt.Println(k)
+
+	var l float32 = 1234
+	fmt.Printf("%v %T\n", l, l)
+
+	var m float64 = 1234
+	fmt.Printf("%v %T\n", m, m)
+
+	fmt.Printf("%v %T\n", n, n)
+
+	var o int = 23 //initialized variable
+	fmt.Printf("%v, %T", o, o)
+
+	var p string
+	p = strconv.Itoa(o)
+	fmt.Printf("%v, %T", p, p)
+
+}
